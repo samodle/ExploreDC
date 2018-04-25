303aeba8d6e2aa0a415d774e5252137009481752
samod1e <sro27@cornell.edu>
2018-04-25 10:58:07 -0400

    fixed back button image issue

--- ExpDC/VRTableViewController.swift
+++ ExpDC/VRTableViewController.swift
@@ -17,15 +17,38 @@ class VRTableViewController: UIViewController {
     //MARK: Variables
     var lastCardClick: FloatingCard = FloatingCard.NA
     @IBOutlet weak var myNavBar: UINavigationItem!
+    @IBOutlet weak var actualNavBarRip: UINavigationBar!
     
     //MARK: View DidLoad/etc
     override func viewDidLoad() {
         super.viewDidLoad()
+/*
+        let navBarbutton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.rewind, target: self, action: #selector(VRTableViewController.LesGoHomeDawg(_:)))
+   */
         
-       // myNavBar.leftBarButtonItem = navigationItem.backBarButtonItem
-        let navBarbutton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.undo, target: self, action: #selector(VRTableViewController.LesGoHomeDawg(_:)))
+        /*
+                let navBarbutton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.rewind, target: self, action: #selector(VRTableViewController.LesGoHomeDawg(_:)))
+        navBarbutton.image = UIImage(named: "LeftArrow_Back")
+        myNavBar.leftBarButtonItem?.image = UIImage(named: "LeftArrow_Back")
         myNavBar.leftBarButtonItem = navBarbutton
+ */
+        let x: CGFloat = 15
+ 
+        let button = UIButton.init(type: .custom)
+        //set image for button
+        button.setImage(UIImage(named: "LeftArrow_Beck"), for: UIControlState.normal)
+        //add function for button
+        button.addTarget(self, action: #selector(VRTableViewController.LesGoHomeDawg(_:)), for: UIControlEvents.touchUpInside)
+        //set frame
+        button.frame = CGRect(x: 0, y: 0, width: x, height: x)
+     //   button.heightAnchor.constraint(equalToConstant: x)// = 25
+     //   button.widthAnchor.constraint(equalToConstant: x)
         
+        let barButton = UIBarButtonItem(customView: button)
+        //assign button to navigationbar
+  
+        myNavBar.leftBarButtonItem = barButton
+    //    myNavBar.leftBarButtonItem?.width = x
         
         //pano init
         mainPanoView.enableFullscreenButton = true
@@ -33,24 +56,31 @@ class VRTableViewController: UIViewController {
         mainPanoView.enableInfoButton = false
         switch lastCardClick{
         case FloatingCard.A:
+            myNavBar.title = "Memorials/Monuments"
         mainPanoView.load(UIImage(named: "union.jpg"),
              of: GVRPanoramaImageType.mono)
         case FloatingCard.B:
+              myNavBar.title = "Government"
             mainPanoView.load(UIImage(named: "garfield00.jpg"),
                               of: GVRPanoramaImageType.mono)
         case FloatingCard.C:
+              myNavBar.title = "Museums"
             mainPanoView.load(UIImage(named: "mall00.jpg"),
                               of: GVRPanoramaImageType.mono)
         case FloatingCard.D:
+              myNavBar.title = "Events & Protests"
             mainPanoView.load(UIImage(named: "taft00.jpg"),
                               of: GVRPanoramaImageType.mono)
         case FloatingCard.E:
+              myNavBar.title = "Cemeteries"
             mainPanoView.load(UIImage(named: "bible01.jpg"),
                               of: GVRPanoramaImageType.mono)
         case FloatingCard.F:
+              myNavBar.title = "Misc."
             mainPanoView.load(UIImage(named: "vets00.jpg"),
                               of: GVRPanoramaImageType.mono)
         case .NA:
+              myNavBar.title = "error"
             mainPanoView.load(UIImage(named: "pano_scusD01.png"),
                               of: GVRPanoramaImageType.mono)
         }
@@ -59,11 +89,6 @@ class VRTableViewController: UIViewController {
     
     
     //MARK: Navigation
-    
-    @IBAction func dismissVC(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
     @objc func LesGoHomeDawg(_ sender:UITapGestureRecognizer){
         dismiss(animated: true, completion: nil)
        // performSegue(withIdentifier: "TableToMainSegue", sender: self)
@@ -72,7 +97,7 @@ class VRTableViewController: UIViewController {
     //not using dis
     func addBackButton() {
         let backButton = UIButton(type: .custom)
-        backButton.setImage(UIImage(named: "LeftArrow_Back.png"), for: .normal)
+        backButton.setImage(UIImage(named: "LeftArrow_Back"), for: .normal)
         backButton.setTitle("  Back", for: .normal)
         backButton.setTitleColor(backButton.tintColor, for: .normal) // You can change the TitleColor
     //    backButton.addTarget(self, action: #selector(self.backAction(_:)),A for: .touchUpInside)
@@ -106,9 +131,7 @@ extension VRTableViewController: GVRWidgetViewDelegate {
         currentDisplayMode = displayMode
         
         if displayMode == GVRWidgetDisplayMode.fullscreen{
-            
         }else{
-            
         }
     }
     
@@ -116,6 +139,5 @@ extension VRTableViewController: GVRWidgetViewDelegate {
         // 1
         guard currentDisplayMode != GVRWidgetDisplayMode.embedded else {return}
         // 2
-        
     }
 }
