e04e05a77bb9f088afc07813ca15ffcab47e4f36
samod1e <sro27@cornell.edu>
2018-04-26 16:25:58 -0400

    done w/ table for now, about to start map revamp

--- ExpDC/SharedFunctions.swift
+++ ExpDC/SharedFunctions.swift
@@ -15,26 +15,26 @@ func getVRTableData(thingy: FloatingCard) -> [PlaceScene]{
     switch thingy{
     case FloatingCard.A:
         iconName = "monu.png"
-        x.append(PlaceScene(title: "Botanical Garden",substit: "this is muh subtitle",photo: "tomb.png",pic2: iconName,photoVR: "bg01.jpg")!)
-         x.append(PlaceScene(title: "Botanical Garden",substit: "this is muh subtitle",photo: "tomb.png",pic2: iconName,photoVR: "bg02.jpg")!)
-         x.append(PlaceScene(title: "Botanical Garden",substit: "this is muh subtitle",photo: "tomb.png",pic2: iconName,photoVR: "bg03.jpg")!)
+        x.append(PlaceScene(title: "Arlington Cemetery",substit: "kilroy was here",photo: "daArlington.png",pic2: iconName,photoVR: "bg01.jpg")!)
+         x.append(PlaceScene(title: "Capitol Building",substit: "this is muh subtitle",photo: "daCapitol.png",pic2: iconName,photoVR: "bg02.jpg")!)
+         x.append(PlaceScene(title: "Botanical Garden",substit: "beep boop beep",photo: "daWwii.png",pic2: iconName,photoVR: "bg03.jpg")!)
     case FloatingCard.B:
         iconName = "capi.png"
-        x.append(PlaceScene(title: "Taft Carrilon",substit: "this is muh subtitle",photo: "tomb.png",pic2: iconName,photoVR: "taft00.jpg")!)
+        x.append(PlaceScene(title: "Taft Carrilon",substit: "this is muh subtitle",photo: "daLinc.png",pic2: iconName,photoVR: "taft00.jpg")!)
     case FloatingCard.C:
         iconName = "muse.png"
-        x.append(PlaceScene(title: "Museum of the Bible",substit: "this is muh subtitle",photo: "capi.png",pic2: iconName,photoVR: "bible00.jpg")!)
-          x.append(PlaceScene(title: "Museum of the Bible",substit: "this is muh subtitle",photo: "tree.png",pic2: iconName,photoVR: "bible01.jpg")!)
-          x.append(PlaceScene(title: "Museum of the Bible",substit: "this is muh subtitle",photo: "prot.png",pic2: iconName,photoVR: "bible02.jpg")!)
+        x.append(PlaceScene(title: "Museum of the Bible",substit: "this is muh subtitle",photo: "daIndians.png",pic2: iconName,photoVR: "bible00.jpg")!)
+          x.append(PlaceScene(title: "Lincoln Memorial",substit: "this is muh subtitle",photo: "daLinc.png",pic2: iconName,photoVR: "bible01.jpg")!)
+          x.append(PlaceScene(title: "Botanical Garden",substit: "this is muh subtitle",photo: "daBotanical.png",pic2: iconName,photoVR: "bible02.jpg")!)
     case FloatingCard.D:
         iconName = "prot.png"
-        x.append(PlaceScene(title: "Women's March",substit: "Feb 27, 2019",photo: "tree.png",pic2: iconName,photoVR: "union.jpg")!)
+        x.append(PlaceScene(title: "Women's March",substit: "Feb 27, 2019",photo: "daWwii.png",pic2: iconName,photoVR: "union.jpg")!)
     case FloatingCard.E:
         iconName = "tomb.png"
-        x.append(PlaceScene(title: "oops",substit: "this is muh subtitle",photo: "capi.png",pic2: iconName,photoVR: "verizon01.jpg")!)
+        x.append(PlaceScene(title: "oops",substit: "this is muh subtitle",photo: "daBotanical.png",pic2: iconName,photoVR: "verizon01.jpg")!)
     case FloatingCard.F:
         iconName = "tree.png"
-        x.append(PlaceScene(title: "The Wharf",substit: "Annual Christmas light boat parade",photo: "prot.png",pic2: iconName,photoVR: "wharf.jpg")!)
+        x.append(PlaceScene(title: "The Wharf",substit: "Annual Christmas light boat parade",photo: "daIndians.png",pic2: iconName,photoVR: "wharf.jpg")!)
     case .NA:
         break
     }
